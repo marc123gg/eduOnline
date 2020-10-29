822535c6ab50c84209f1d3c3b97b0b029c3480ef
zengjianming <zjm123456>
2020-10-29 17:22:01 +0800

    初始化

---
+++ src/test/java/com/jm/eduonline/EduonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.eduonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
